d450d51c9ecd032905405898965de0a9500aab66
rgopal56749 <115881753+rgopal56749@users.noreply.github.com>
2022-10-16 13:46:27 +0530

    Create Large-element.c

---
+++ Large-element.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+ 
+
+int main()
+
+{
+
+    int size, i, largest;
+
+ 
+
+    printf("\n Enter the size of the array: ");
+
+    scanf("%d", &size);
+
+    int array[size];
+
+ 
+
+ 
+
+ 
+
+    printf("\n Enter %d elements of the array: \n", size);
+
+ 
+
+    for (i = 0; i < size; i++)
+
+    {   
+
+        scanf(" %d", &array[i]);
+
+    }
+
+ 
+
+    largest = array[0];
+
+ 
+
+    for (i = 1; i < size; i++) 
+
+    {
+
+        if (largest < array[i])
+
+        largest = array[i];
+
+    }
+
+ 
+
+    printf("\n largest element present in the given array is : %d", largest);
+
+ 
+
+    return 0;
+
+ }
